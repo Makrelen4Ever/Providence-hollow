66afa407d4bae4220e80e412a7d46a4d98974340
Makrelen4Ever <147800392+Makrelen4Ever@users.noreply.github.com>
2024-09-19 19:56:52 +0200

    Reimported the project
    I reimported the project and fixed the crouching

--- Providence-hollow files/Providence-hollow game files/Assets/Scripts/GenericMovement.cs
+++ Providence-hollow files/Providence-hollow game files/Assets/Scripts/GenericMovement.cs
@@ -20,6 +20,11 @@ public class GenericMovement : MonoBehaviour
 
     [Space]
 
+    public float TiredWalkSpeed;
+    public float MaxTiredWalkSpeed;
+
+    [Space]
+
     public float StaminaGain;
     public float StaminaWaste;
 
@@ -38,6 +43,17 @@ public class GenericMovement : MonoBehaviour
     public float JumpForce;
     public float JumpStaminaUsage;
 
+    [Header("Crouching")]
+    public float CrouchHeight;
+    public float CrouchSpeed;
+    public float CrouchMaxSpeed;
+
+    [Space]
+
+    public KeyCode CrouchKey;
+
+    private bool IsCrouching;
+
     [Header("Gravity")]
     public float Gravity;
 
@@ -45,8 +61,10 @@ public class GenericMovement : MonoBehaviour
 
     [Header("Collision")]
     public float CollisionDistance;
+    public float CollisionDistanceCrouching;
     public LayerMask GroundMask;
     public Transform GroundCheckTransform;
+    public Transform GroundCheckTransformCrouching;
 
     private bool IsGrounded;
 
@@ -59,7 +77,8 @@ public class GenericMovement : MonoBehaviour
     void Update(){
 
         //Ground check
-        IsGrounded = Physics.CheckSphere(GroundCheckTransform.position, CollisionDistance, GroundMask);
+        IsGrounded = IsCrouching ? Physics.CheckSphere(GroundCheckTransformCrouching.position, CollisionDistanceCrouching, GroundMask) : Physics.CheckSphere(GroundCheckTransform.position, CollisionDistance, GroundMask);
+
 
         //Gravity
         if(IsGrounded && GravityVelocity.y < 0){
@@ -69,11 +88,29 @@ public class GenericMovement : MonoBehaviour
         GravityVelocity.y -= Gravity * Time.deltaTime;
         CC.Move(GravityVelocity * Time.deltaTime);
 
-        //Moving
+        //Check if crouching
+        if(!IsRunning && IsGrounded && Input.GetKeyDown(CrouchKey)){
+            IsCrouching = !IsCrouching;
+            // if(IsCrouching){
+            //     Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 2, GroundMask);
+            //     transform.position = hit.point + new Vector3(0, 1, 0);
+            // }else{
+            //     for(var i = 0; i <= 60; i++){
+            //         if(IsCrouching){
+            //             break;
+            //         }
+            //         transform.position -= new Vector3(0, 0.025f, 0);
+            //     }
+            //     transform.position += new Vector3(0, 0.025f, 0);
+            // }
+        }
+
+        //Check if running
         IsRunning = Input.GetKey(KeyCode.LeftShift) && Stamina > 0 && CanRun;
         Stamina += IsRunning ? StaminaWaste * Time.deltaTime : StaminaGain * Time.deltaTime;
         Stamina = Mathf.Clamp(Stamina, 0, MaxStamina);
 
+        //Checks if the player have enough stamina
         if(Stamina <= 0){
             CanRun = false;
         }else{
@@ -82,16 +119,27 @@ public class GenericMovement : MonoBehaviour
             }
         }
 
+        // sets the scale of the stamina bar in the corner
         StaminaBar.transform.localScale = new Vector3(Stamina/MaxStamina, 1, 1);
         if(ColorEnabled){StaminaBar.GetComponent<Image>().color = CanRun ? CanRunColor : CannotRunColor;}
 
+        //Getting move directions
         InputAxis = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         MovementAxis = InputAxis.x * transform.right + InputAxis.z * transform.forward;
-    
-        CC.Move(MovementAxis * (IsRunning ? RunSpeed : WalkSpeed) * Time.deltaTime);
+
+        //Starts moving
+        if(IsCrouching){
+            CC.Move(MovementAxis * CrouchSpeed * Time.deltaTime);
+            transform.localScale = new Vector3(1, CrouchHeight, 1);
+
+        }else{
+            transform.localScale = new Vector3(1, 1, 1);
+            CC.Move(MovementAxis * (IsRunning ? RunSpeed : WalkSpeed) * Time.deltaTime);
+        }
 
         //Jumping
         if(Input.GetKeyDown("space") && IsGrounded && CanRun && Stamina >= JumpStaminaUsage){
+            IsCrouching = false;
             GravityVelocity = new Vector3(GravityVelocity.x, JumpForce, GravityVelocity.z);
             Stamina -= JumpStaminaUsage;
         }else{
@@ -102,6 +150,7 @@ public class GenericMovement : MonoBehaviour
     }
 
     IEnumerator ColorFlash(){
+        //Flash the stamina bar if you dont have enough stamina
         ColorEnabled = false;
         StaminaBar.GetComponent<Image>().color = CannotRunColor;
         yield return new WaitForSeconds(.05f);
